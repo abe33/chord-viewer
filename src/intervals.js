959c95c388e2914a6d973aad1c53bf02a0bffab4
abe33 <cedric.nehemie@gmail.com>
2016-02-11 15:25:44 +0100

    Implement isInterval and isIntervalName functions

--- src/intervals.js
+++ src/intervals.js
@@ -1,14 +1,39 @@
 import R from 'ramda'
 import notes from './notes'
 
+const {all, anyPass, both, compose, curry, equals, last, length, test} = R
+
 const {isNote, normalize, transposeBySemitone} = notes
 
-const isOctave = R.gte(R.__, 0)
+const positiveNumber = R.gte(R.__, 0)
+
+const isInterval = both(compose(equals(2), length), all(isNote))
+
+const onlyOnIntervalOrDefault = curry((def, continuation) => {
+  return R.curryN(continuation.length, (...args) => {
+    return isInterval(last(args)) ? continuation(...args) : def
+  })
+})
+
+const onlyOnInterval = onlyOnIntervalOrDefault('')
+
+const isIntervalName = anyPass([
+  test(/^(aug|A)([12456])$/),
+  test(/^(dim|d)([45])$/),
+  test(/^(maj|M)([2367])$/),
+  test(/^(min|m)([2367])$/),
+  test(/^(perf|P)([45])$/)
+])
+
 
 const semitoneInterval = (interval, note) => {
-  return isOctave(interval) && isNote(note)
+  return positiveNumber(interval) && isNote(note)
     ? [normalize(note), transposeBySemitone(interval, note)]
     : []
 }
 
-export default {semitoneInterval}
+export default {
+  isInterval,
+  isIntervalName,
+  semitoneInterval
+}
--- test/intervals.js
+++ test/intervals.js
@@ -2,6 +2,68 @@ import expect from 'expect.js'
 import intervals from '../src/intervals'
 
 describe('intervals', () => {
+  describe('.isInterval()', () => {
+    it('matches a tuple of notes', () => {
+      expect(intervals.isInterval([])).to.be(false)
+      expect(intervals.isInterval(['A'])).to.be(false)
+      expect(intervals.isInterval(['I', 'B'])).to.be(false)
+      expect(intervals.isInterval(['A', 'I'])).to.be(false)
+
+      expect(intervals.isInterval(['A', 'B'])).to.be(true)
+      expect(intervals.isInterval(['D#4', 'Eb5'])).to.be(true)
+    })
+  })
+
+  describe('isIntervalName()', () => {
+    it('matches valid interval long names', () => {
+      expect(intervals.isIntervalName('aug1')).to.be(true)
+      expect(intervals.isIntervalName('aug2')).to.be(true)
+      expect(intervals.isIntervalName('aug4')).to.be(true)
+      expect(intervals.isIntervalName('aug5')).to.be(true)
+      expect(intervals.isIntervalName('aug6')).to.be(true)
+      expect(intervals.isIntervalName('dim4')).to.be(true)
+      expect(intervals.isIntervalName('dim5')).to.be(true)
+      expect(intervals.isIntervalName('maj2')).to.be(true)
+      expect(intervals.isIntervalName('maj3')).to.be(true)
+      expect(intervals.isIntervalName('maj6')).to.be(true)
+      expect(intervals.isIntervalName('maj7')).to.be(true)
+      expect(intervals.isIntervalName('min2')).to.be(true)
+      expect(intervals.isIntervalName('min3')).to.be(true)
+      expect(intervals.isIntervalName('min6')).to.be(true)
+      expect(intervals.isIntervalName('min7')).to.be(true)
+      expect(intervals.isIntervalName('perf4')).to.be(true)
+      expect(intervals.isIntervalName('perf5')).to.be(true)
+    })
+
+    it('matches valid interval short names', () => {
+      expect(intervals.isIntervalName('A1')).to.be(true)
+      expect(intervals.isIntervalName('A2')).to.be(true)
+      expect(intervals.isIntervalName('A4')).to.be(true)
+      expect(intervals.isIntervalName('A5')).to.be(true)
+      expect(intervals.isIntervalName('A6')).to.be(true)
+      expect(intervals.isIntervalName('d4')).to.be(true)
+      expect(intervals.isIntervalName('d5')).to.be(true)
+      expect(intervals.isIntervalName('m2')).to.be(true)
+      expect(intervals.isIntervalName('M2')).to.be(true)
+      expect(intervals.isIntervalName('m3')).to.be(true)
+      expect(intervals.isIntervalName('M3')).to.be(true)
+      expect(intervals.isIntervalName('m6')).to.be(true)
+      expect(intervals.isIntervalName('M6')).to.be(true)
+      expect(intervals.isIntervalName('m7')).to.be(true)
+      expect(intervals.isIntervalName('M7')).to.be(true)
+      expect(intervals.isIntervalName('P4')).to.be(true)
+      expect(intervals.isIntervalName('P5')).to.be(true)
+    })
+
+    it('does not match invalid interval names', () => {
+      expect(intervals.isIntervalName('A8')).to.be(false)
+      expect(intervals.isIntervalName('I')).to.be(false)
+      expect(intervals.isIntervalName('d3')).to.be(false)
+      expect(intervals.isIntervalName('M4')).to.be(false)
+      expect(intervals.isIntervalName('P3')).to.be(false)
+    })
+  })
+
   describe('.semitoneInterval()', () => {
     it('returns an array that forms the interval', () => {
       expect(intervals.semitoneInterval(-1, 'A')).to.eql([])
