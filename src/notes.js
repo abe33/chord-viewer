07909ef9f6b8633cb280f9d61e8039f246ac9989
abe33 <cedric.nehemie@gmail.com>
2016-03-05 13:09:35 +0100

    :memo: Add functional signatures on notes functions

--- src/notes.js
+++ src/notes.js
@@ -5,36 +5,72 @@ const {
   add, all, always, apply, compose, cond, converge, curry, defaultTo, drop, equals, either, head, indexOf, join, keys, last, length, map, prop, replace, split, subtract, test, times, toLower, toUpper, unapply
 } = R
 
+/**
+ * `String -> Boolean`
+ */
 const isNote = test(new RegExp(`^${C.NOTE_REGEXP.source}$`, 'i'))
 
+/**
+ * `* -> (Note -> *) -> (String -> *)`
+ */
 const onlyOnNoteOrDefault = curry((def, continuation) => {
   return R.curryN(continuation.length, (...args) => {
     return isNote(last(args)) ? continuation(...args) : def
   })
 })
 
+/**
+ * `* -> ([Note] -> *) -> ([String] -> *)`
+ */
 const onlyOnNotesOrDefault = curry((def, continuation) => {
   return R.curryN(continuation.length, (...args) => {
     return all(isNote, args) ? continuation(...args) : def
   })
 })
 
+/**
+ * `(Note -> *) -> (String -> *)`
+ */
 const onlyOnNote = onlyOnNoteOrDefault('')
 
+/**
+ * `Note -> String`
+ */
 const letter = compose(toUpper, replace(/[^A-G]/i, ''), head)
 
+/**
+ * `Note -> String`
+ */
 const accidental = compose(toLower, replace(/^.|[^Bb#]$/gi, ''))
 
+/**
+ * `Note -> Number`
+ */
 const octave = compose(Number, defaultTo(C.MIDDLE_OCTAVE), last, drop(1), split(/(?=\d)/))
 
+/**
+ * `Note -> Note`
+ */
 const name = onlyOnNote(converge(unapply(join('')), [letter, accidental]))
 
+/**
+ * `Note -> Note`
+ */
 const normalize = onlyOnNote(converge(unapply(join('')), [letter, accidental, octave]))
 
+/**
+ * `Number -> Number`
+ */
 const double = R.multiply(2)
 
+/**
+ * `Number -> Number`
+ */
 const half = R.divide(R.__, 2)
 
+/**
+ * `Number -> Number -> Number -> [(Number -> Number)]`
+ */
 const pitchModulator = (base) => {
   const iterate = cond([
     [R.lt(0), compose(times(always(half)), Math.abs)],
@@ -44,46 +80,79 @@ const pitchModulator = (base) => {
   return compose(iterate, subtract(base))
 }
 
+/**
+ * `Number -> Number -> [(Number -> Number)]`
+ */
 const pitchModulators = R.uncurryN(2, pitchModulator)
 
+/**
+ * `Number -> Object -> Note -> Number`
+ */
 const pitchTransposer = onlyOnNote((base, frequencies, note) => {
   const modulatePitch = compose(...pitchModulators(base, octave(note)))
 
   return modulatePitch(prop(name(note), frequencies))
 })
 
+/**
+ * `Note -> Number`
+ */
 const frequency = pitchTransposer(C.MIDDLE_OCTAVE, C.PITCH_BY_NAME)
 
+/**
+ * `Object -> String -> String`
+ */
 const substitute = (map) => {
   const re = new RegExp(join('|', keys(map)))
 
   return (note) => replace(re, m => map[m], note)
 }
 
+/**
+ * `Note -> String`
+ */
 const display = compose(
   substitute(C.ACCIDENTALS_DISPLAY_MAP),
   substitute(C.OCTAVE_DISPLAY_MAP),
   normalize
 )
 
+/**
+ * `Note -> Note`
+ */
 const variant = prop(R.__, C.SEMITONE_CONVERSION)
 
+/**
+ * `Note -> Note -> Boolean`
+ */
 const noteEquals = curry((a, b) => {
   const compare = either(equals(name(a)), equals(variant(name(a))))
 
   return compare(name(b))
 })
 
+/**
+ * `Note -> Note -> Boolean`
+ */
 const pitchEquals = curry((a, b) => {
   return equals(frequency(a), frequency(b))
 })
 
+/**
+ * `Note -> Note`
+ */
 const transposeToOctave = onlyOnNote((octave, note) => {
   return `${name(note)}${octave}`
 })
 
+/**
+ * `[a] -> Number`
+ */
 const lastIndex = compose(subtract(R.__, 1), R.length)
 
+/**
+ * `[Note] -> [Note, Number] -> Note`
+ */
 const nextNoteInScale = curry((scale, [note, octave]) => {
   return cond([
     [R.equals(lastIndex(scale)), always([scale[0], octave + 1])],
@@ -91,6 +160,9 @@ const nextNoteInScale = curry((scale, [note, octave]) => {
   ])(indexOf(note, scale))
 })
 
+/**
+ * `[Note] -> [Note, Number] -> Note`
+ */
 const previousNoteInScale = curry((scale, [note, octave]) => {
   return cond([
     [R.equals(0), always([last(scale), octave - 1])],
@@ -98,6 +170,9 @@ const previousNoteInScale = curry((scale, [note, octave]) => {
   ])(indexOf(note, scale))
 })
 
+/**
+ * `[Note] -> Number -> Note -> Note`
+ */
 const transposeInScale = onlyOnNote((scale, offset, note) => {
   const steps = cond([
     [R.gt(0), compose(times(always(previousNoteInScale(scale))), Math.abs)],
@@ -110,18 +185,39 @@ const transposeInScale = onlyOnNote((scale, offset, note) => {
   return walk([name(note), octave(note)])
 })
 
+/**
+ * `Number -> Note -> Note`
+ */
 const transposeBySemitone = transposeInScale(C.CHROMATIC_SCALE)
 
+/**
+ * `[Note] -> Number -> Number`
+ */
 const wrapDistanceInScale = curry((scale, v) => v < 0 ? v + length(scale) : v)
 
+/**
+ * `Number -> Number`
+ */
 const wrapDiatonicDistance = wrapDistanceInScale(C.DIATONIC_SCALE)
 
+/**
+ * `Number -> Number`
+ */
 const wrapChromaticDistance = wrapDistanceInScale(C.CHROMATIC_SCALE)
 
+/**
+ * `Note -> Number`
+ */
 const diatonicIndex = prop(R.__, C.DIATONIC_INDEX_MAP)
 
+/**
+ * `Note -> Number`
+ */
 const chromaticIndex = prop(R.__, C.CHROMATIC_INDEX_MAP)
 
+/**
+ * `Note -> Note -> Number`
+ */
 const diatonicDistance = onlyOnNotesOrDefault(-1, (a, b) => {
   const find = compose(diatonicIndex, letter)
   const distance = compose(add(1), wrapDiatonicDistance, apply(subtract), map(find))
@@ -129,6 +225,9 @@ const diatonicDistance = onlyOnNotesOrDefault(-1, (a, b) => {
   return distance([b, a])
 })
 
+/**
+ * `Note -> Note -> Number`
+ */
 const chromaticDistance = onlyOnNotesOrDefault(-1, (a, b) => {
   const distance = compose(wrapChromaticDistance, apply(subtract), map(chromaticIndex))
 
