ddf5489262e7151c0b30e41e2c228aa369c2ce08
abe33 <cedric.nehemie@gmail.com>
2016-07-08 00:50:46 +0200

    Add dummy chords module

---
+++ src/chords.js
@@ -0,0 +1,5 @@
+import R from 'ramda'
+import notes from './notes'
+
+
+export default {}
---
+++ test/chords.js
@@ -0,0 +1,6 @@
+import expect from 'expect.js'
+import chords from '../src/chords'
+
+describe('chords', () => {
+  
+})
